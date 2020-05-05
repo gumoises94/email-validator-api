9d63ebc0208aa73d738506e92637d84e5718230c
gumoises <gumoises@live.com>
2020-05-05 15:50:28 -0300

    initial commit

---
+++ src/main/java/com/gumoises94/emailvalidatorapi/EmailValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.gumoises94.emailvalidatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailValidatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gumoises94/emailvalidatorapi/EmailValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumoises94.emailvalidatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailValidatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
